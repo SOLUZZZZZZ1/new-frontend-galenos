c864932d547c5d953d929a3941b9e01fd5e49c52
Ramón <ramon@spainroom.es>
2025-11-27 16:43:20 +0100

    Galenos.pro frontend inicial

---
+++ README.txt
@@ -0,0 +1,1 @@
+Configura VITE_NEWS_API y añade la ruta /actualidad a tu router. Copia src/lib/newsApi.js y src/pages/Actualidad.jsx.
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
