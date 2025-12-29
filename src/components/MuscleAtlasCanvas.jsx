8ead2da7b713da5a6dcc2969059760ddbb904b7f
mediazion <soluzziona@gmail.com>
2025-12-29 12:51:34 +0100

    Add files via upload

--- src/components/MuscleAtlasCanvas.jsx
+++ src/components/MuscleAtlasCanvas.jsx
@@ -14,7 +14,7 @@ import React from "react";
  */
 export default function MuscleAtlasCanvas({
   src,
-  anatomyBox = { x0: 10, y0: 10, x1: 95, y1: 74 },
+  anatomyBox = { x0: 10, y0: 10, x1: 95, y1: 78 },
   layerPercents = { skinEnd: 0.06, subcEnd: 0.22, fasciaEnd: 0.28, muscleStart: 0.44 },
   className = "",
 }) {
