3349b5b5cec266a84e36a9fe4ac2e2ef200498bc
mediazion <soluzziona@gmail.com>
2025-12-29 11:25:26 +0100

    Add files via upload

--- src/components/MuscleAtlasCanvas.jsx
+++ src/components/MuscleAtlasCanvas.jsx
@@ -20,7 +20,7 @@ import React from "react";
  */
 export default function MuscleAtlasCanvas({
   src,
-  anatomyBox = { x0: 10, y0: 12, x1: 95, y1: 86 },
+  anatomyBox = { x0: 10, y0: 10, x1: 95, y1: 92 },
   className = "",
 }) {
   if (!src) return null;
